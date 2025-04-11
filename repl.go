c5649455d58aef4bc5c403d899c2685470b363b1
Ben <benjaminafoster4@gmail.com>
2025-04-11 10:10:18 -0400

    added exit and help commands to repl flow

--- repl.go
+++ repl.go
@@ -18,9 +18,24 @@ func startRepl() {
 			continue
 		}
 
-		command := cleanInputText[0]
+		// get the first word from the cleaned input which is the command
+		commandName := cleanInputText[0]
 
-		fmt.Printf("Your command was: %s\n", command)
+		commandRegistry := getCommands()
+
+		command, exists := commandRegistry[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Unknown command")
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		
 	}
 }
 
@@ -28,4 +43,25 @@ func cleanInput(text string) []string {
 	lowerCaseText := strings.ToLower(text)
 	words := strings.Fields(lowerCaseText)
 	return words
+}
+
+type cliCommand struct {
+	name                string
+	description         string
+	callback            func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:           "exit",
+			description:    "Exit the Pokedex",
+			callback:       commandExit,
+		},
+		"help": {
+			name:           "help",
+			description:    "Displays a help message",
+			callback:       commandHelp,
+		},
+	}
 }
